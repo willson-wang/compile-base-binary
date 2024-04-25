43f6c17e6a71f5fe4b484db7c552619c6229c580
wangks <wangks@mingyuanyun.com>
2024-04-25 17:16:45 +0800

    feat: 调试windows模式2

--- grpc/windows-latest-build.bat
+++ grpc/windows-latest-build.bat
@@ -9,7 +9,7 @@ echo "开始编译二进制文件"
 rmdir node_modules
 
 # 安装 grpc，如果二进制不存在，则会走编译流程
-call yarn add grpc@1.24.11
+call yarn add grpc@1.24.11 || goto :error
 
 # 进入 grpc 的目录
 cd .\node_modules\grpc\src\node\extension_binary
